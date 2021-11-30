1ae8b34c997677fa5b3d9de3a7b1014718e66f71
Emily <emilydta@users.noreply.github.com>
2021-11-30 17:03:24 +1100

    fixed div mouseover functionality, removd rset btn

--- etch.js
+++ etch.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 256; i++) {
     newGridSquare.setAttribute("class", "grid-square");
 }
 
-const gridSquares = document.querySelectorAll(".grid-square");
+let gridSquares = document.querySelectorAll(".grid-square");
 gridSquares.forEach(gridSquare => {
     gridSquare.addEventListener("mouseover", ()=>{
         gridSquare.style.backgroundColor="black";
@@ -12,18 +12,31 @@ gridSquares.forEach(gridSquare => {
  });
 
 const resetButton = document.createElement('button');
-buttonContainer = document.getElementById("button-container");
-buttonContainer.appendChild(resetButton);
-resetButton.setAttribute("type", "button");
-resetButton.innerText="Reset Grid";
+
+
 
 function resetGrid() {
     gridSquares.forEach(gridSquare => {
         gridSquare.remove();
     });
+    let userInput = prompt("Enter a number from 1-100 to generate a new grid.");
+    newGridDimensions = userInput*userInput; 
+
+    for (let i = 0; i < newGridDimensions; i++) {
+        let gridSquares = document.querySelectorAll(".grid-square");
+        const newGridSquare = document.createElement('div'); 
+        container.appendChild(newGridSquare);
+        newGridSquare.setAttribute("class", "grid-square");
+        newGridSquare.addEventListener("mouseover", ()=>{
+            newGridSquare.style.backgroundColor="black"
+        });
+    };
 };
 
+
+
 resetButton.addEventListener("click", () => resetGrid());    
+
 
 
 
