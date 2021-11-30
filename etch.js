3939917b229e5f577be52121b24a90e4596051a6
Emily <emilydta@users.noreply.github.com>
2021-12-01 10:47:12 +1100

    Added dynamic grid functionality and grid reqs

--- etch.js
+++ etch.js
@@ -1,17 +1,23 @@
+const errorMessage = document.getElementById("error-message");
+const resetButton = document.querySelector('button');
+const errorText = document.createElement('p');
+
 const createGrid = (boxes) => {
     for (let i = 0; i < boxes; i++) {
+        let columns = Math.sqrt(boxes);
+        let rows = Math.sqrt(boxes);
+        container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         const newGridSquare = document.createElement('div'); 
         container.appendChild(newGridSquare);
         newGridSquare.setAttribute("class", "grid-square");
         newGridSquare.addEventListener("mouseover", () => {
             newGridSquare.style.backgroundColor="black";
         });
-    }
+    };
 };
 
 createGrid(256);
-
-
 
 function removeSquares() {
     let gridSquares = document.querySelectorAll(".grid-square");
@@ -22,10 +28,18 @@ function removeSquares() {
 
 function resetGrid() {
     removeSquares();
-    let userInput = prompt("Enter a number from 1-100 to generate a new grid.");
-    newGridDimensions = userInput*userInput; 
-    createGrid(newGridDimensions);
-}
+    let userInput = prompt("Enter a number from 2-100 to generate a new grid.");
+    if (userInput > 100 || userInput < 2){
+        errorMessage.appendChild(errorText);
+        errorText.innerText = "You did not choose a number between 2-100. Please try again."
+    } else {
+        errorText.remove();
+        newGridDimensions = userInput*userInput; 
+        createGrid(newGridDimensions);
+    }
+};
 
-const resetButton = document.querySelector('button');
 resetButton.addEventListener("click", () => resetGrid());    
+
+
+
